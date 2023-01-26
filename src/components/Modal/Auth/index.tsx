15577093af6d15df38f36c2473f338d82b851079
JeanDes-Code <78590821+JeanDes-Code@users.noreply.github.com>
2023-01-26 12:32:12 +0100

    UI - Login/Signup Modals

--- src/components/Modal/Auth/index.tsx
+++ src/components/Modal/Auth/index.tsx
@@ -1,35 +1,61 @@
 import React from 'react';
 import {
-  Button,
+  Flex,
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
-  ModalFooter,
   ModalHeader,
+  Text,
   ModalOverlay,
-  useDisclosure,
 } from '@chakra-ui/react';
+import { useRecoilState } from 'recoil';
+import { authModalState } from '@/atoms/authModalAtom';
+import AuthInputs from './AuthInputs';
+import OAuthButtons from './OAuthButtons';
 
-const AuthModal = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+const AuthModal: React.FC = () => {
+  const [modalState, setModalState] = useRecoilState(authModalState);
+
+  const handleClose = () => {
+    setModalState((prev) => ({
+      ...prev,
+      open: false,
+    }));
+  };
   return (
     <>
-      <Button onClick={onOpen}>Open Modal</Button>
-
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={modalState.open} onClose={handleClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader textAlign="center">
+            {modalState.view === 'login' && 'Login'}
+            {modalState.view === 'signup' && 'Sign Up'}
+            {modalState.view === 'resetPassword' && 'Reset Password'}
+          </ModalHeader>
           <ModalCloseButton />
-          <ModalBody>Here is the modal body</ModalBody>
+          <ModalBody
+            display="flex"
+            flexDir="column"
+            alignItems="center"
+            justifyContent="center"
+            pb={6}
+          >
+            <Flex
+              direction="column"
+              align="center"
+              justify="center"
+              width="70%"
+            >
+              <OAuthButtons />
+              <Text color="gray.500" fontWeight={700}>
+                OR{' '}
+              </Text>
+              <AuthInputs />
 
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button variant="ghost">Secondary Action</Button>
-          </ModalFooter>
+              {/*      <ResetPassword/>    */}
+            </Flex>
+          </ModalBody>
         </ModalContent>
       </Modal>
     </>
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,7 +2,7 @@ import { Flex, Image } from '@chakra-ui/react';
 import RightContent from './RightContent';
 import SearchInput from './SearchInput';
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
   return (
     <Flex bg="white" height="44px" padding="6px 12px">
       <Flex align="center">
